3370db0a1712676dc05632f61bf3899526ae3391
Alexander Ajiduku <mutaborax@yahoo.com>
2023-08-28 19:01:19 +0200

    Promises Exercise Solution Part 2 Async Await

--- part_1/script.js
+++ part_1/script.js
@@ -1,33 +1,83 @@
-// Part 1: Number Facts
+// // Part 1: Number Facts
+// const numbersAPI = "http://numbersapi.com";
+
+// // 1. Get a fact about your favorite number
+// let favoriteNumber = 7;
+// fetch(`${numbersAPI}/${favoriteNumber}?json`)
+//   .then(response => response.json())
+//   .then(data => console.log(data.text))
+//   .catch(error => console.error(error));
+
+// // 2. Get data on multiple numbers in a single request
+// let numbers = [1, 2, 3, 4, 5];
+// fetch(`${numbersAPI}/${numbers}?json`)
+//   .then(response => response.json())
+//   .then(data => {
+//     for (let number in data) {
+//       console.log(`${number}: ${data[number]}`);
+//     }
+//   })
+//   .catch(error => console.error(error));
+
+// // 3. Get 4 facts about your favorite number
+// let promises = [];
+// for (let i = 0; i < 4; i++) {
+//   promises.push(fetch(`${numbersAPI}/${favoriteNumber}?json`));
+// }
+
+// Promise.all(promises)
+//   .then(responses => Promise.all(responses.map(response => response.json())))
+//   .then(data => {
+//     data.forEach((fact, index) => console.log(`Fact ${index + 1}: ${fact.text}`));
+//   })
+//   .catch(error => console.error(error));
+
+
+  // Part 1: Number Facts
 const numbersAPI = "http://numbersapi.com";
 
 // 1. Get a fact about your favorite number
-let favoriteNumber = 7;
-fetch(`${numbersAPI}/${favoriteNumber}?json`)
-  .then(response => response.json())
-  .then(data => console.log(data.text))
-  .catch(error => console.error(error));
+let favoriteNumber = 12;
+async function fetchFavoriteNumber() {
+    try {
+        let response = await fetch(`${numbersAPI}/${favoriteNumber}?json`);
+        let data = await response.json();
+        console.log(data.text);
+    } catch (error) {
+        console.error(error);
+    }
+}
+fetchFavoriteNumber();
 
 // 2. Get data on multiple numbers in a single request
-let numbers = [1, 2, 3, 4, 5];
-fetch(`${numbersAPI}/${numbers}?json`)
-  .then(response => response.json())
-  .then(data => {
-    for (let number in data) {
-      console.log(`${number}: ${data[number]}`);
+let numbers = [1, 2, 3, 4, 5, 6, 8, 10];
+async function fetchMultipleNumbers() {
+    try {
+        let response = await fetch(`${numbersAPI}/${numbers}?json`);
+        let data = await response.json();
+        for (let number in data) {
+            console.log(`${number}: ${data[number]}`);
+        }
+    } catch (error) {
+        console.error(error);
     }
-  })
-  .catch(error => console.error(error));
+}
+fetchMultipleNumbers();
 
 // 3. Get 4 facts about your favorite number
 let promises = [];
 for (let i = 0; i < 4; i++) {
-  promises.push(fetch(`${numbersAPI}/${favoriteNumber}?json`));
+    promises.push(fetch(`${numbersAPI}/${favoriteNumber}?json`));
 }
 
-Promise.all(promises)
-  .then(responses => Promise.all(responses.map(response => response.json())))
-  .then(data => {
-    data.forEach((fact, index) => console.log(`Fact ${index + 1}: ${fact.text}`));
-  })
-  .catch(error => console.error(error));
+async function fetchFacts() {
+    try {
+        let responses = await Promise.all(promises);
+        let data = await Promise.all(responses.map(response => response.json()));
+        data.forEach((fact, index) => console.log(`Fact ${index + 1}: ${fact.text}`));
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+fetchFacts();
--- part_2/cards.js
+++ part_2/cards.js
@@ -1,38 +1,87 @@
-const deckAPI = "https://deckofcardsapi.com/api/deck";
-const drawCardButton = document.getElementById('draw-card');
-const cardImage = document.getElementById('card-image');
+// const deckAPI = "https://deckofcardsapi.com/api/deck";
 
+// const drawCardButton = document.getElementById('draw-card');
+// const cardImage = document.getElementById('card-image');
+
+// cardImage.style.display = "none"; // Ensure that image is initially hidden
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     drawCardButton.addEventListener('click', () => {
+//         cardImage.style.display = "none"; // Hide image each time the button is clicked
+//     });
+// });
+
+// fetch(`${deckAPI}/new/shuffle/?deck_count=1`)
+//   .then(response => response.json())
+//   .then(data => {
+//     let deckId = data.deck_id;
+//     drawCardButton.addEventListener('click', () => drawCard(deckId));
+//   })
+//   .catch(error => console.error(error));
+
+// function drawCard(deckId) {
+//   fetch(`${deckAPI}/${deckId}/draw/?count=1`)
+//     .then(response => response.json())
+//     .then(data => {
+//       if (data.remaining === 0) {
+//         alert('No cards left in the deck!');
+//         drawCardButton.disabled = true;
+//       } else {
+//         let card = data.cards[0];
+//         cardImage.src = card.image;
+//         cardImage.style.display = "block"; // Card drawn, show the image
+//       }
+//     })
+//     .catch(error => console.error(error));
+// }
+
+const deckAPI = "https://deckofcardsapi.com/api/deck";
+
+const drawCardButton = document.getElementById('draw-card');
+const cardContainer = document.getElementById('card-container');
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('draw-card').addEventListener('click', () => {
-        // Assuming that 'response' is obtained from some API or function
-        if (response) {
-            document.getElementById('card-image').style.display = "block";
-        } else {
-            document.getElementById('card-image').style.display = "none";
-        }
+    drawCardButton.addEventListener('click', () => {
+        cardContainer.innerHTML = ''; // clear the container
     });
 });
 
-fetch(`${deckAPI}/new/shuffle/?deck_count=1`)
-  .then(response => response.json())
-  .then(data => {
-    let deckId = data.deck_id;
-    drawCardButton.addEventListener('click', () => drawCard(deckId));
-  })
-  .catch(error => console.error(error));
+let cardArray = [];
 
-function drawCard(deckId) {
-  fetch(`${deckAPI}/${deckId}/draw/?count=1`)
-    .then(response => response.json())
-    .then(data => {
-      if (data.remaining === 0) {
-        alert('No cards left in the deck!');
-        drawCardButton.disabled = true;
-      } else {
-        let card = data.cards[0];
-        cardImage.src = card.image;
-      }
-    })
-    .catch(error => console.error(error));
+async function getDeckId() {
+    try {
+        let response = await fetch(`${deckAPI}/new/shuffle/?deck_count=1`);
+        let data = await response.json();
+        let deckId = data.deck_id;
+        drawCardButton.addEventListener('click', () => drawCard(deckId));
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+getDeckId();
+
+async function drawCard(deckId) {
+  try {
+      let response = await fetch(`${deckAPI}/${deckId}/draw/?count=1`);
+      let data = await response.json();
+      if (data.remaining === 0) {
+          alert('No cards left in the deck!');
+          drawCardButton.disabled = true;
+      } else {
+          let card = data.cards[0];
+          cardArray.push(card); // add card to the end of the array
+          cardContainer.innerHTML = ''; // clear the container
+          cardArray.forEach((c, index) => {
+              let img = document.createElement('img');
+              img.src = c.image;
+              img.style.position = 'absolute';
+              img.style.transform = `rotate(${index * 5}deg)`; // apply rotation
+              cardContainer.appendChild(img);
+          });
+      }
+  } catch (error) {
+      console.error(error);
+  }
+}
+
